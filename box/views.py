79b200cf1cf124009243a04915a173fe8014391c
Alchrist <alchrist@student.21-school.ru>
2021-06-17 22:35:04 +0300

    delete unused code

--- box/views.py
+++ box/views.py
@@ -5,19 +5,11 @@ from rest_framework.mixins import  CreateModelMixin
 from .models import *
 from .serializers import *
 
-# class OrderViewSet(viewsets.ModelViewSet):
-#     queryset = Order.objects.all()
-#     serializer_class = OrderSerializer
 
 class OrderCreateSet(CreateModelMixin, GenericViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    # def perform_create(self, serializer):
-    #     author = get_object_or_404(CoffeePoint, id=self.request.data.get('author_id'))
-    #     return serializer.save(author=author)
-
 
 class CoffeePointSet(ReadOnlyModelViewSet):
     queryset = CoffeePoint.objects.prefetch_related('coffee').all()
